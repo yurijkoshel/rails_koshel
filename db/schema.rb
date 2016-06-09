9b241547abb3e1fe94226712ea184f58659e7230
Yurij <yurij.koshel@gmail.com>
2016-06-09 13:32:07 +0300

    Add Gitignore, routes.rb, schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603071817) do
+ActiveRecord::Schema.define(version: 20160607162847) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
