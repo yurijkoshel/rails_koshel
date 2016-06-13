1477859e94d6639494569dd84176df9a5e22c45d
Yurij <yurij.koshel@gmail.com>
2016-06-13 16:15:43 +0300

    Init files

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,5 @@
 class Cart < ActiveRecord::Base
   has_many :products
   belong_to :account
-
-    create_table :cart do |t|
-      t.string :name
-      t.text :description
-      t.float :price
     end
-  end
+
